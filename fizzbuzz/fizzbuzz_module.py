22ac477d761cab10b2be435eb2c96e09a97d2b4e
hamargyuri <gyurinak@gmail.com>
2016-05-30 11:42:24 +0200

    fizzbuzz test-ok

--- fizzbuzz/fizzbuzz_module.py
+++ fizzbuzz/fizzbuzz_module.py
@@ -1,8 +1,18 @@
 def fizzbuzz(number):
-    return
+    print_num = number
+    if number % 15 == 0:
+        print_num = "FizzBuzz"
+    elif number % 3 == 0:
+        print_num = "Fizz"
+    elif number % 5 == 0:
+        print_num = "Buzz"
+    return print_num
 
 
 def main():
+    for i in range(1, 101):
+        fizzbuzz(i)
+        print(fizzbuzz(i))
     return
 
 if __name__ == '__main__':
