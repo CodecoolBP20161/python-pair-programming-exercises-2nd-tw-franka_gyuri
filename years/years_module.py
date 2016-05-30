edb75fd602562c5e6202579662721b84741e5074
hamargyuri <gyurinak@gmail.com>
2016-05-30 13:35:40 +0200

    years test ok

--- years/years_module.py
+++ years/years_module.py
@@ -2,11 +2,16 @@ import datetime
 
 
 def years(age):
-    return
+    now_date = datetime.datetime.now()
+    when_100 = now_date.year + 100 - age
+    return when_100
 
 
 def main():
-    return
+    name = input("What's your name?\n")
+    age = int(input("How old are you?\n"))
+    how_many = int(input("How many times shall I print the answer?"))
+    print(("\nAt this time of year %s you'll be 100 years old." % (years(age))) * how_many)
 
 
 if __name__ == '__main__':
