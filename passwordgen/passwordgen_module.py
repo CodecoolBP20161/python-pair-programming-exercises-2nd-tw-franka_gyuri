292b976e9a5933959b008edc00f00bf9f83a58da
b-franka <b.franku@gmail.com>
2016-06-02 14:10:20 +0200

    password test ok

--- passwordgen/passwordgen_module.py
+++ passwordgen/passwordgen_module.py
@@ -1,8 +1,28 @@
-def passwordgen():
-    return
+import random
+
+
+def passwordgen(length=8):
+    length = int(input("Pick a length for your password(min 8 characters)!\n"))
+    if length < 8:
+        length = int(input("Password should be minimum 8 characters you freak!\nChose another!\n"))
+    character_lists = ['abcdefghijklmnopqrstuvwxyz', 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', '0123456789', '!@#$%^&*()?']
+    while True:
+        password = ''
+        used = [False, False, False, False]
+        all_used = [True, True, True, True]
+        for i in range(length):
+            a = random.randint(0, 3)
+            used[a] = True
+            s = character_lists[a]
+            b = random.randint(0, len(s) - 1)
+            password += s[b]
+        if used == all_used:
+            break
+    return password
 
 
 def main():
+    print(passwordgen())
     return
 
 
